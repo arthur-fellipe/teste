b55c95da39263d837d78027c6e740bb811b773b6
root <root@LAPTOP-7N4H458J>
2022-12-28 05:32:58 +0000

    Implementa script-init.sh para criação de estrutura de diretórios completa a partir de arquivos .tsv

--- script-init.sh
+++ script-init.sh
@@ -1,2 +1,68 @@
 #!/bin/bash
 
+
+# mkdir estrutura_teste
+# paste -d'/' teste.txt teste_anos.txt > resultado.txt
+# paste -d'/' teste.txt teste_anos.txt teste_conjuntos.txt > resultado_conjuntos.txt
+
+mkdir estrutura_completa
+
+cut -f4 sets.tsv > temas.txt 
+cut -f3 sets.tsv > anos.txt
+cut -f2 sets.tsv > conjuntos.txt
+cut -f1 sets.tsv > set_num.txt
+
+paste -d'/' temas.txt anos.txt > temas_anos.txt
+paste -d'/' temas.txt anos.txt conjuntos.txt > temas_anos_conjuntos.txt
+# | sed -i 's/ /\\ /g' temas.txt
+
+IFS=;
+# while  read -r linha;
+# do
+#     if [ "$linha" != "theme" -a "$linha" != "year" ];
+#     then
+#         mkdir estrutura_teste/$linha
+#     fi
+# done < teste.txt
+
+# while  read -r linha;
+# do
+#     if [ "$linha" != "theme/year" ];
+#     then
+#         mkdir estrutura_teste/$linha
+#     fi
+# done < resultado.txt
+
+# while  read -r linha;
+# do
+#     if [ "$linha" != "theme/year/name" ];
+#     then
+#         mkdir estrutura_teste/$linha
+#     fi
+# done < resultado_conjuntos.txt
+
+while read -r linha;
+do
+  if [ "$linha" != "theme" ];
+   then
+    mkdir estrutura_completa/$linha
+   fi
+done < temas.txt
+
+while  read -r linha;
+do
+    if [ "$linha" != "theme/year" ];
+    then
+        mkdir estrutura_completa/$linha
+    fi
+done < temas_anos.txt
+
+while  read -r linha;
+do
+    if [ "$linha" != "theme/year/name" ];
+    then
+        mkdir estrutura_completa/$linha
+    fi
+done < temas_anos_conjuntos.txt
+
+# APAGAR ARQUIVOS TXT
--- script.sh
+++ script.sh
@@ -6,38 +6,49 @@ echo "Pasta descomprimida com sucesso!"
 
 #Cria pasta com novo tema na estrutura
 mkdir primeira/Easter
-echo "Pasta criada com sucesso!"
+echo "Pasta Easter criada com sucesso!"
 
 #Cria pastas com novos anos dentro da pasta Easter
 mkdir primeira/Easter/2008 primeira/Easter/2010
-echo "Pastas criadas com sucesso!"
+echo "Pastas 2008 e 2010 criadas com sucesso!"
 
 #Lista ficheiros na pasta primeira/Harry Potter/2004/Knight Bus - Mini
-ls primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini
+echo "Ficheiros na pasta Knight Bus - Mini: "
+ls -l primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini
+echo " "
 
 #Exibe conteúdo do ficheiro Bricks
+echo "Ficheiro Bricks:"
 cat primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Bricks
+echo " "
 
 #Mescla conteúdo de todos os ficheiros da pasta primeira/Harry Potter/2004/Knight Bus - Mini
 cat primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Bricks primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Plates primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Wheels\ and\ Tyres > primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Pecas
-echo "Ficheiros mesclados com sucesso!"
+echo "Ficheiros na pasta Knight Bus - Mini mesclados com sucesso!"
 
 #Exibe conteúdo do ficheiro Peças
+echo "Ficheiro Peças:"
 cat primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Pecas
+echo " "
 
 #Conta o número de caracteres do ficheiro Peças
-echo "Número de caracteres: "
+echo "Número de caracteres do ficheiro Peças: "
 wc -m primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Pecas
+echo " "
 
 #Exibe conteúdo do ficheiro Peças em ordem alfabética descendente
+echo "Ficheiro Peças em ordem alfabética descendente: "
 sort -r primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Pecas
+echo " "
 
-#Apaga ficheiros da pasta Harry Potter
-rm primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Bricks primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Plates primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Wheels\ and\ Tyres > primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Pecas
-echo "Ficheiros deletados com sucesso!"
+#Apaga ficheiros da pasta primeira/Harry Potter/2004/Knight Bus - Mini
+rm primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Bricks primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Plates primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini/Wheels\ and\ Tyres
+echo "Ficheiros 'Bricks', 'Plates' e 'Wheels and Tyres' deletados com sucesso!"
 
 #Lista ficheiros na pasta primeira/Harry Potter/2004/Knight Bus - Mini
-ls primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini
+echo "Ficheiros na pasta Knight Bus - Mini: "
+ls -l primeira/Harry\ Potter/2004/Knight\ Bus\ -\ Mini
+echo " "
 
 #Comprime a pasta primeira/ modificada
 zip -r segunda.zip primeira/
