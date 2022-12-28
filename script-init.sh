75c4a531dfc1b4aa49caa9eb61d27883b0fefaf1
root <root@LAPTOP-7N4H458J>
2022-12-28 12:48:00 +0000

    Salva alterações

--- script-init.sh
+++ script-init.sh
@@ -6,17 +6,20 @@
 # paste -d'/' teste.txt teste_anos.txt teste_conjuntos.txt > resultado_conjuntos.txt
 
 mkdir estrutura_completa
-
-cut -f4 sets.tsv > temas.txt 
-cut -f3 sets.tsv > anos.txt
+IFS=;
 cut -f2 sets.tsv > conjuntos.txt
-cut -f1 sets.tsv > set_num.txt
+cut -f3 sets.tsv > anos.txt
+cut -f4 sets.txt > temas.txt | sed -i 's,/, ,g' temas.txt
+# cut -f2 temas.txt > temas2.txt
+# cut -f1 sets.tsv > set_num.txt
+ sed -i 's,$,/,g' temas.txt
 
-paste -d'/' temas.txt anos.txt > temas_anos.txt
-paste -d'/' temas.txt anos.txt conjuntos.txt > temas_anos_conjuntos.txt
+paste -d'/' temas.txt anos.txt  > temas_anos.txt
+paste -d'/' anos.txt conjuntos.txt > anos_conjuntos.txt
+# paste -d'/' set_num.txt temas.txt anos.txt conjuntos.txt > num_temas_anos_conjuntos.txt
 # | sed -i 's/ /\\ /g' temas.txt
 
-IFS=;
+
 # while  read -r linha;
 # do
 #     if [ "$linha" != "theme" -a "$linha" != "year" ];
@@ -41,28 +44,36 @@ IFS=;
 #     fi
 # done < resultado_conjuntos.txt
 
-while read -r linha;
-do
-  if [ "$linha" != "theme" ];
-   then
-    mkdir estrutura_completa/$linha
-   fi
-done < temas.txt
+# while read -r linha;
+# do
+#   if [ "$linha" != "theme" ];
+#    then
+#     mkdir estrutura_completa/$linha
+#    fi
+# done < temas.txt
 
-while  read -r linha;
-do
-    if [ "$linha" != "theme/year" ];
-    then
-        mkdir estrutura_completa/$linha
-    fi
-done < temas_anos.txt
+# while  read -r linha ano;
+# do
+#     if [ "$linha" != "year" ];
+#     then
+#         mkdir estrutura_completa/$linha/$ano
+#     fi
+# done < temas.txt anos.txt
 
-while  read -r linha;
-do
-    if [ "$linha" != "theme/year/name" ];
-    then
-        mkdir estrutura_completa/$linha
-    fi
-done < temas_anos_conjuntos.txt
+# while  read -r linha;
+# do
+#     if [ "$linha" != "theme/year" ];
+#     then
+#         mkdir estrutura_completa/$linha
+#     fi
+# done < temas_anos.txt
+
+# while  read -r linha;
+# do
+#     if [ "$linha" != "theme/year/name" ];
+#     then
+#         mkdir estrutura_completa/$linha
+#     fi
+# done < temas_anos_conjuntos.txt
 
 # APAGAR ARQUIVOS TXT
